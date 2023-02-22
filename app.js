008eb29f483d2f7e9676f9e5c005c0a234ad80fc
André Kleine <andrekleine@gmail.com>
2023-02-22 19:28:21 +0100

    Finished

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+// Paths
 app.get('/', (req, res) => {
-  const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-  res.sendFile(htmlFilePath);
+  res.render('index');
 });
 
 app.get('/restaurants', (req, res) => {
-  const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
-  res.sendFile(htmlFilePath);
-});
+  const jsonFilePath = path.join(__dirname, 'data', 'restaurants.json');
+  const fileData = fs.readFileSync(jsonFilePath);
+  const storedRestaurants = JSON.parse(fileData);
 
-app.get('/about', (req, res) => {
-  const htmlFilePath = path.join(__dirname, 'views', 'about.html');
-  res.sendFile(htmlFilePath);
-});
-
-app.get('/confirm', (req, res) => {
-  const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
-  res.sendFile(htmlFilePath);
+  res.render('restaurants', {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
 });
 
 app.get('/recommend', (req, res) => {
-  const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
-  res.sendFile(htmlFilePath);
+  res.render('recommend');
 });
 
-app.get('/restaurants', (req, res) => {
-  const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
-  res.sendFile(htmlFilePath);
+app.post('/recommend', (req, res) => {
+  const restaurant = req.body;
+  const jsonFilePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(jsonFilePath);
+  const storedRestaurants = JSON.parse(fileData);
+  storedRestaurants.push(restaurant);
+  fs.writeFileSync(jsonFilePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirm');
+});
+
+app.get('/confirm', (req, res) => {
+  res.render('confirm');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 app.listen(3000);
